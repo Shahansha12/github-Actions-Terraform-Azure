d70d81654aef7269fcff57d2fcfda42ea6d26586
Shahansha12 <139757163+Shahansha12@users.noreply.github.com>
2024-02-14 14:14:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    resource_group_name  = "R1"
+    storage_account_name = "terraformazures"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
